ba9fbb74399134433fc09ce6db29de02402550db
ChristianLiu <christianliu@college.harvard.edu>
2014-09-21 02:04:48 -0400

    form to create guest, multiple select for times creates new guests for each time

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -1,14 +1,24 @@
 class GuestsController < ApplicationController
   def new
     @guest = Guest.new
+    @times = TimeSlot.all
   end
 
   def create
-  	@guest = Guest.create(guest_params)
+  	params["guest"]["time_slot_id"].delete_if(&:blank?).each { |id|
+      if Guest.new(guest_params).update_attributes(time_slot_id: id)
+        flash[:success] = "Application successfully submitted."
+      else
+        flash[:error] = "There was an error with your application."
+      end
+    }
+    redirect_to root_path
   end
 
   private
   def guest_params    
-    params.require(:guest).permit(:name, :email, :phone, :description)
+    params.require(:guest).permit(:name, :email, :phone, :description, :time_slot_id)
   end
 end
+
+
